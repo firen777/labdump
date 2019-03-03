6a371c7562e7da7ce62a1c5a080d5833f19260f6
Albert Chan <hivemindac1995@gmail.com>
2019-03-03 09:25:38 +0800

    main add stuff

--- backbonejs-lab/main.js
+++ backbonejs-lab/main.js
@@ -1,17 +1,72 @@
-
-TestModel = Backbone.Model.extend({
+let TestModel = Backbone.Model.extend({
+  urlRoot: 'http://localhost:3003/user',
+  default:{
+    "id": "0",
+    "username": "user0",
+    "password": "password0",
+    "greeting": "helloworld0",
+    "gender": "male",
+  },
   initialize: function() {
       console.log('backbone model works')
   },
 });
 
 
-// let testModel1 = new TestModel();
-
-
-$(document).ready(()=>{
-    console.log('jquery works');
-
-    let testModel1 = new TestModel();
+let testModel1 = new TestModel({
+  "id": "1",
+  "username": "user1",
+  "password": "password1",
+  "greeting": "helloworld1",
+  "gender": "male",
 });
+let testModel2 = new TestModel({
+  "id": "2",
+  "username": "user2",
+  "password": "password2",
+  "greeting": "helloworld2",
+  "gender": "female",
+});
+
+testModel1.save({
+  "id": "1",
+  "username": "user1",
+  "password": "password1",
+  "greeting": "helloworld1",
+  "gender": "male",
+}, {
+  success: function (user) {
+    alert(JSON.stringify(user));
+}})
+
+
+// $(document).ready(()=>{
+//     console.log('jquery works');
+
+//     let testModel1 = new TestModel({
+//       "id": "1",
+//       "username": "user1",
+//       "password": "password1",
+//       "greeting": "helloworld1",
+//       "gender": "male",
+//     });
+//     let testModel2 = new TestModel({
+//       "id": "2",
+//       "username": "user2",
+//       "password": "password2",
+//       "greeting": "helloworld2",
+//       "gender": "female",
+//     });
+
+//     testModel1.save({
+//       "id": "1",
+//       "username": "user1",
+//       "password": "password1",
+//       "greeting": "helloworld1",
+//       "gender": "male",
+//     }, {
+//       success: function (user) {
+//         alert(JSON.stringify(user));
+//     }})
+// });
 
